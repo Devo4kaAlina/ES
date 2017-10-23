72da4c878ec71b9e363aa0f7891c8fe4950d6b0b
Sergey <hardslimlife@mail.ru>
2017-10-23 09:20:28 +0300

    frs less

--- 1-lesson_String/index.js
+++ 1-lesson_String/index.js
@@ -5,3 +5,33 @@ var string = 'i love javascript'
 // 3. вывести в консоль строку 'tpircsavaj evol i'
 // 4. заменить слово javascript на слово ECMAScript
 // 5. вывести в консоль второе слово
+for (var i = 0; i < string.length ; i++) {
+	console.log('1 -----------:' + string.charAt(i))
+};
+/////////////
+function toUpper(str) {
+	var newStr = '';
+	newStr = str.replace(str[0],str[0].toUpperCase());
+	for (var i = 0; i < newStr.length; i++) {
+		if(newStr[i] === ' ') {
+			newStr = newStr.replace(newStr.charAt(i + 1),newStr.charAt(i + 1).toUpperCase());
+		}
+	};
+	
+	console.log('2 ----------' + newStr)
+}
+toUpper(string)
+////////////
+function reverse(string) {
+    var newStr = '';
+    for (var i = string.length - 1; i >= 0; i--) {
+        newStr += string.charAt(i);
+    }
+    console.log('3 ----------' + newStr)
+}
+reverse(string)
+/////
+
+console.log('4 -----------' + string.replace('javascript','ECMAScript'))
+///////
+console.log('5----------' + string.split(' ')[1])
