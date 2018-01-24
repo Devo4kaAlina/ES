6beab7db3e49520c2e4b2df670ed98e89ff3926c
Sergey <hardslimlife@mail.ru>
2018-01-24 09:10:59 +0200

    Check home work

--- 3-lesson_Object/index.js
+++ 3-lesson_Object/index.js
@@ -4,18 +4,18 @@
 
 
 var object = {
-  name: "Вася",
-  age: 25,
-}
-/**
- * №1
- */
+        name: "Вася",
+        age: 25,
+    }
+    /**
+     * №1
+     */
 console.log(checkProp(object, 'age'))
 
 function checkProp(obj, value) {
-  for (let key in obj) {
-  	if (key == value) return true
-  }
+    for (let key in obj) {
+        if (key == value) return true
+    }
 }
 
 
@@ -25,12 +25,12 @@ function checkProp(obj, value) {
 console.log(getObjectKeys(object))
 
 function getObjectKeys(obj) {
-	let arr = [];
-  for (let key in obj) {
-  	arr.push(key)
-  }
-  // return arr.toString() or
-  return arr.join()
+    let arr = [];
+    for (let key in obj) {
+        arr.push(key)
+    }
+    // return arr.toString() or
+    return arr.join()
 }
 
 
@@ -40,60 +40,78 @@ function getObjectKeys(obj) {
 console.log(getObjectLength(object))
 
 function getObjectLength(obj) {
-	let count = 0;
-	for (let key in obj) {
-		count ++;
-	}
-  return [count, Object.keys(obj).length]
+    let count = 0;
+    for (let key in obj) {
+        count++;
+    }
+    return [count, Object.keys(obj).length]
 }
 
 /**
  * №4
  */
- var array = [
- 	1, 2, 'three',
-  { name: "Вася"  },
-  { name: "Masha" },
-  { name: "Sergey"},
-  {display: "block", color: 'red'}
+var array = [
+    1, 2, 'three',
+    { name: "Вася" },
+    { name: "Masha" },
+    { name: "Sergey" },
+    { display: "block", color: 'red' }
 ]
 console.log(cloneArray(array))
 
 function cloneArray(arr) {
-	let clonArr = [];
-	for (let i = 0; i < arr.length; i++) {
-		clonArr[i] = arr[i]
-	};
-  return clonArr
+    let clonArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        clonArr[i] = arr[i]
+    };
+    return clonArr
 }
+
+/**
+ * а что будет если я создам клон массива и запешу его в переменную "clone"
+ * потом поменяю исходный массив array.
+ * что я увижу если выведу clone[3].name?
+ * где глубокое клонирование?
+ */
+var clone = cloneArray(array);
+array[3].name = false;
+console.log(clone[3].name);
 
 
 /**
  * №5
  */
- var  utils = {
- 	firstChar: function(str) {return str.charAt(0)},
- 	lastChar: function(str) {return str.charAt(str.length - 1)},
- 	reverseStr: function(str) {
- 		return str.split('').reverse().join('')
- 	},
- 	infoStr: function(str) {
- 		return {size: str.length ,spaceLessSize: str.replace(" ","").length}
- 	},
- 	toUpper: function(str) {
- 		return str.toUpperCase()
- 	}
- };
+var utils = {
+    firstChar: function(str) { return str.charAt(0) },
+    lastChar: function(str) { return str.charAt(str.length - 1) },
+    reverseStr: function(str) {
+        return str.split('').reverse().join('')
+    },
+    infoStr: function(str) {
+        return { size: str.length, spaceLessSize: str.replace(" ", "").length }
+    },
+    toUpper: function(str) {
+        return str.toUpperCase()
+    }
+};
 
-console.log('firstChar = ', processString('Hello', 'firstChar'))
-console.log('lastChar = ', processString('Hello', 'lastChar'))
-console.log('infoStr = ', processString('Hello world', 'infoStr'))
-console.log('reverseStr = ', processString('Hello world', 'reverseStr'))
-console.log('without METHOD = ', processString('Hello world'))
+console.log('firstChar = ', processString1('Hello', 'firstChar'))
+console.log('lastChar = ', processString1('Hello', 'lastChar'))
+console.log('infoStr = ', processString1('Hello world', 'infoStr'))
+console.log('reverseStr = ', processString1('Hello world', 'reverseStr'))
+console.log('without METHOD = ', processString1('Hello world'))
 
 function processString(string, met) {
-	method = utils[met] ? utils[met] : utils.toUpper;
+    method = utils[met] ? utils[met] : utils.toUpper;
     return method(string)
+}
+
+/**
+ * хорошо!
+ * можно и не записывать в переменную
+ */
+function processString1(string, met) {
+    return met ? utils[met](string) : utils.toUpper(string)
 }
 // ======================================================
 //              Part 2
@@ -101,20 +119,20 @@ function processString(string, met) {
 
 
 var objectName = {
-  name: "Вася",
-  surname: "Pypkin",
-  age: 25,
-}
-/**
- * №1
- */
+        name: "Вася",
+        surname: "Pypkin",
+        age: 25,
+    }
+    /**
+     * №1
+     */
 Object.defineProperties(objectName, {
- 	'name' : {
- 		writable: false
- 	},
- 	'surname' : {
- 		writable: false
- 	}
+    'name': {
+        writable: false
+    },
+    'surname': {
+        writable: false
+    }
 })
 objectName.name = 'Petya';
 console.log('defineProperties edit=', objectName.name)
@@ -128,39 +146,55 @@ console.log('Object.name = ', objectName.name)
  * №2
  */
 var objectPrice = {
-  count: 2, 
-  price: 15,
-  toString: function() {
-  	return this.count*this.price
-  }
+    count: 2,
+    price: 15,
+    toString: function() {
+        return this.count * this.price
+    }
 }
 Object.defineProperty(objectPrice, 'toString', {
-  enumerable: false
+    enumerable: false
 });
 console.log('objectPrice.toString()', objectPrice.toString())
-
-
-
+for (var key in objectPrice) {
+    console.log('>>>> key ', key)
+}
 
 /**
  * №3
  */
 
-function setPropertiesObject(obj,arrSet) {
-	if (!arrSet.length) return obj
-	var setProp = {};
-	for (let i = 0; i < arrSet.length; i++) {
-		setProp[arrSet[i]] = false
-	}
-	let keys = Object.keys(obj);
-	for (let i = 0; i < keys.length; i++) {
-		console.log(keys[i])
-		Object.defineProperty(obj, keys[i] ,setProp)
-	}
-	return obj
+function setPropertiesObject(obj, arrSet) {
+    if (!arrSet.length) return obj
+    var setProp = {};
+    for (let i = 0; i < arrSet.length; i++) {
+        setProp[arrSet[i]] = false
+    }
+    let keys = Object.keys(obj);
+    for (let i = 0; i < keys.length; i++) {
+        console.log(keys[i])
+        Object.defineProperty(obj, keys[i], setProp)
+    }
+    return obj
+}
+/**
+ * не нужно создавать лишний массив
+ * используй то, что уже встроено нативно -- метод перебора ключей объекта
+ * мне нравится, что ты сразу определил setProp, а не гоняешь arrSet для каждого ключа :)
+ */
+function setPropertiesObject1(obj, arrSet) {
+    if (!arrSet.length) return obj
+    var setProp = {};
+    for (let i = 0; i < arrSet.length; i++) {
+        setProp[arrSet[i]] = false
+    }
+    for (let key in obj) {
+        Object.defineProperty(obj, key, setProp)
+    }
+    return obj
 }
 
-console.log('setPropertiesObject', setPropertiesObject(objectPrice,['writable','enumerable']))
+console.log('setPropertiesObject', setPropertiesObject1(objectPrice, ['writable', 'enumerable']))
 objectPrice.count = 555;
 objectPrice.price = 555;
 console.log('objectPrice.count=', objectPrice.count, 'objectPrice.price=', objectPrice.price)
