87d2e140086fdc6a6dd53a23327f792dd1277ae9
Alina Devonina <alina.devonina@gmail.com>
2018-08-31 16:54:46 +0300

    clear code

--- 2-lesson_Array/index.js
+++ 2-lesson_Array/index.js
@@ -1,368 +1,74 @@
-///////////////////----------- PART1 --------------//////////
-console.log('///////////////////----------- PART1 --------------//////////')
-
-
-// double array
-function doubleArr(arr) {
-	let newArray = []
-	for (let i = 0; i < arr.length; i++) {
-		newArray.push(arr[i])
-	};
-	return arr.concat(newArray)
-}
-console.log('double array =', doubleArr([1,2,3]))
-
-console.log('=====================================')
-console.log('без создания нового массива -- ', (function (array) {
-  return array.concat(array)
-})([1,2,3]))
-console.log('=====================================')
-// double array
-
-
-
-// get last elem
-function getLast(arr) {
-	return arr[arr.length - 1]
-}
-console.log('get last elem =', getLast([1,2,3,4,5]))
-console.log('=====================================')
-console.log('методом .pop() -- ', (function (array) { return array.pop()})([1,2,3,4,5]))
-console.log('=====================================')
-// get last elem
-
-
-
-// create Array
-function createArr(n) {
-	let arr = []
-	let i = 0
-	while (i < n) {
-		arr[i] = i + 1
-		i++
-	}
-	return arr
-}
-console.log('create Array =', createArr(15))
-// create Array
-
-
-
-// change array
-function changeArrays() {
-	var args = Array.prototype.slice.call(arguments)
-	args.forEach(function(item, i, arr) {
-		args[i].shift()
-	})
-	return args
-}
-console.log('change array =', changeArrays(['a','b','c','d'], ['1','2','3','4','5','6'], [9,8,7,6,5]))
-// change array
-///////////////////----------- PART1 --------------//////////
-///////////////////----------- PART2 --------------//////////
-console.log('///////////////////----------- PART2 --------------//////////')
-// sort and reverse string
-function sortReverseStr(str) {
-	console.log('string init = ', str)
-	let arrStr = []
-	for (let i = 0; i < str.length ; i++) {
-		arrStr.push(str.charAt(i))
-	};
-	arrStr.sort(function(a,b) {
-		if (a < b) return -1;
-		if (a > b) return 1
-	})
-	arrStr.reverse()
-	return arrStr.join('')
-}
-console.log('1.sort and reverse string =', sortReverseStr('qwertyuiopasdfghjklzxcvbnm'))
-console.log('====================================')
-console.log('не переберая строку и не пушай в новый массив -- ', 
-'qwertyuiopasdfghjklzxcvbnm'
-  .split('')
-  .sort(function(a,b) {
-		if (a < b) return -1;
-		if (a > b) return 1
-  })
-  .reverse()
-  .join(''))
-/**
- * каждый из примененных методов,
- * кроме .join() возвращает тебе массив, 
- * с которым ты дальше можешь работать 
- * и применять к нему методы, создавай цепь
- */
-console.log('====================================')
-// sort and reverse string
-
-
-
-// sort array
-function sortArray(arr) {
-	arr.sort(function(a,b) {
-		return a - b;
-	})	
-	return arr
-}
-console.log('2.sort array =', sortArray([2,56,-4,-105,7,9531,5,-100,-1]))
-// sort array
-
-
-
-// get new array
-function getNewArray(arr, start, end) {
-	let newArr = []
-	newArr = arr.slice(start, end)
-	return newArr
-}
-console.log('3.get new array =', getNewArray(['a','b','c','d','e','f'], 1, 3))
-// get new array
-
-
-
-// double array
-function doubleArray(arr) {
-	return arr.concat(arr)
-}
-console.log('4.double array =', doubleArray(['a','b']))
-// double array
-
-
-
-//remove element
-function remEl(arr, start, col) {
-	arr.splice(2,2)
-	return arr
-}
-console.log('5.remove 2,3 element =', remEl([1,2,3,4,5], 2,2))
-//remove element
-
-
-
-//remove element and replace
-function remElRep(arr, start, col) {
-	arr.splice(2,2,'three','four')
-	return arr
-}
-console.log('6.remove element and replace =', remElRep([1,2,3,4,5], 2,2))
-//remove element and replace
-
-
-
-//add in array
-function addInArray(arr, whrehe, what) {
-	arr.splice(whrehe,0,what)
-	return arr
-}
-console.log('7.add in array =', addInArray([1,2,3,4,5], 2, 'test'))
-//add in array
-
-
-
-//sort arrays
-function sortArrays(arrays) {
-	arrays.sort(function(arr,arr2){
-		return arr.length - arr2.length
-	})
-	return arrays
-}
-console.log('8.sort arrays for length=', sortArrays([[1,2,3,4,5],[2],['s','b','s']]))
-/**
- * давай будем учиться давать функциям более осмысленные названия
- * тебе здесь нужно отсортировать массивы по их длинне
- * "sortArrays" - уже неплохое начало, можно было бы добавить еще название параметра,
- * по которому сортируешь
- * например: sortArraysByLength 
- */
-//sort arrays
-
-
-
-//copy array
-function copyArray(array) {
-	return array.concat(array)
-}
-console.log('9.copy array=', copyArray([1,2,3,4,5]))
-//copy array
-
-
-
 //sort array obj
-function sortArrayObj(obj) {
-	obj.sort(objSort)
-	function objSort (a,b) {
-		if (a.info.cores < b.info.cores) return -1;
-		if (a.info.cores > b.info.cores) return 1;
-	}
-	return obj
-}
 const arrObj = [
-	{
-		cpu: 'intel',
-		info: {
-			cores: 2,
-			cashe: 3
-		}
-	},
-	{
-		cpu: 'AMD',
-		info: {
-			cores: 4,
-			cashe: 4
-		}
-	},
-	{
-		cpu: 'intel',
-		info: {
-			cores: 1,
-			cashe: 4
-		}
-	},
-	{
-		cpu: 'amd',
-		info: {
-			cores: 8,
-			cashe: 4
-		}
-	},
-	{
-		cpu: 'intel',
-		info: {
-			cores: 6,
-			cashe: 4
-		}
-	}
-]
-console.log('10.sort array obj=', sortArrayObj(arrObj))
-//sort array obj
-
-
+  {
+    cpu: "intel",
+    info: {
+      cores: 2,
+      cashe: 3
+    }
+  },
+  {
+    cpu: "AMD",
+    info: {
+      cores: 4,
+      cashe: 4
+    }
+  },
+  {
+    cpu: "intel",
+    info: {
+      cores: 1,
+      cashe: 4
+    }
+  },
+  {
+    cpu: "amd",
+    info: {
+      cores: 8,
+      cashe: 4
+    }
+  },
+  {
+    cpu: "intel",
+    info: {
+      cores: 6,
+      cashe: 4
+    }
+  }
+];
 
 // filter product
 const products = [
-	{title: 'prod1', price: 15},
-	{title: 'prod2', price: 105},
-	{title: 'prod3', price: 1215},
-	{title: 'prod4', price: 18},
-	{title: 'prod5', price: 165},
-	{title: 'prod6', price: 615},
-	{title: 'prod7', price: 215},
-	{title: 'prod8', price: 515},
-	{title: 'prod9', price: 315},
-	{title: 'prod10', price: 855},
-]
-
-function filterProducts(products, from, to) {
-	newProdList = []
-	products.sort(function(a,b){
-		return a.price - b.price
-	})
-	products.forEach(function(item, i){
-		if (products[i].price > from && products[i].price < to) {
-			newProdList.push(products[i])
-		} 
-	})
-	return newProdList
-}
-console.log('11.filter product=', filterProducts(products, 400, 1000))
-// filter product
-
-
-
-///////////////////----------- PART3 --------------//////////
-console.log('///////////////////----------- PART3 --------------//////////')
-// filter product
-console.log('1.filter product=', filterProd(products, 400, 1000))
-function filterProd(products, from, to) {
-	products.sort(function(a,b){
-		return a.price - b.price
-	})
-	function filter(element) {
-	    return (element.price > from && element.price < to)
-	}
-	let newProd = products.filter(filter)
-	return newProd
-}
-/**
- * function filterProd(products, from, to) {
-      products.sort(function(a,b){
-        return a.price - b.price
-      })
-      let newProd = products.filter((e) => filter(e, from, to))
-      return newProd
-    }
-    function filter(element, from, to) {
-        return (element.price > from && element.price < to)
-    }
- */
-
-
-
-
-// transform array
-console.log('2.transform array=', transformArr([1,2,3,5,8,9,10,24,56]))
-function transformArr(arr) {
-	let newArr = []
-	arr.map(function(el){
-		return newArr.push({digit: el, odd: el % 2 === 0})
-	})
-	return newArr
-}
-// check array on 0
-console.log('3.check array on 0=', [1,2,3,0,5,8,9,10,24,56].every(checkArr))
-function checkArr(el) {
-  return el !== 0;
-}
-console.log('3.check array on 0= SOME ', ![1,2,3,0,5,8,9,10,24,56].some((el) => !el))
-console.log('3.check array on 0= EVERY ', [1,2,3,0,5,8,9,10,24,56].every((el) => !el))
-
-
-
-// check array on wordLength
-console.log('4.check array on wordLength=', ['yes', 'no', 'hello', 'javascript'].some(wordLength))
-function wordLength(el) {
-  return el.length > 4
-}
-
-
+  { title: "prod1", price: 15 },
+  { title: "prod2", price: 105 },
+  { title: "prod3", price: 1215 },
+  { title: "prod4", price: 18 },
+  { title: "prod5", price: 165 },
+  { title: "prod6", price: 615 },
+  { title: "prod7", price: 215 },
+  { title: "prod8", price: 515 },
+  { title: "prod9", price: 315 },
+  { title: "prod10", price: 855 }
+];
 
 // string from array
 var strObj = [
-	{char: 'a', index: 12},
-	{char: 'w', index: 8},
-	{char: 'Y', index: 10},
-	{char: 'p', index: 3},
-	{char: 'p', index: 2},
-	{char: 'N', index: 6},
-	{char: '', index: 5},
-	{char: 'y', index: 4},
-	{char: 'r', index: 13},
-	{char: 'H', index: 0},
-	{char: 'e', index: 11},
-	{char: 'a', index: 1},
-	{char: '', index: 9},
-	{char: '!', index: 14},
-	{char: 'e', index: 7}
+  { char: "a", index: 12 },
+  { char: "w", index: 8 },
+  { char: "Y", index: 10 },
+  { char: "p", index: 3 },
+  { char: "p", index: 2 },
+  { char: "N", index: 6 },
+  { char: "", index: 5 },
+  { char: "y", index: 4 },
+  { char: "r", index: 13 },
+  { char: "H", index: 0 },
+  { char: "e", index: 11 },
+  { char: "a", index: 1 },
+  { char: "", index: 9 },
+  { char: "!", index: 14 },
+  { char: "e", index: 7 }
 ];
-console.log('5.string from array=', stringToArray(strObj))
-function stringToArray(arr) {
-	arr.sort(function(a,b){
-		return (a.index < b.index) ? -1 : 1
-	});
-	var res = arr.reduce(function(res,curr){
-		return res + curr.char
-	},'')
-	return res
-}
-console.log('5.string from array=', stringToArray2(strObj))
-function stringToArray2(arr) {
-	var a = [];
-  strObj.forEach(obj => (a[obj.index] = obj.char));
-  return a.join('');
-}
-
-
-
 
 var people = [
   {
@@ -727,25 +433,35 @@ var people = [
   }
 ];
 Array.prototype.diff = function(a) {
-    return this.filter(function(i) {return a.indexOf(i) < 0;});
+  return this.filter(function(i) {
+    return a.indexOf(i) < 0;
+  });
 };
 function sortPeople(peop) {
-	let peopCheckList = peop.filter(function(el){
-		return el.status.indexOf('some') > -1 || el.status.indexOf('apple') > -1 || el.status.indexOf('green') > -1 || el.status.indexOf('white') > -1   
-	})
-	let peopYoung = peopCheckList.filter(function(year) {
-		return year.age < 35
-	})
-	let peopOld = peopCheckList.filter(function(year) {
-		return year.age > 35
-	})
-	let peopOther = peop.diff(peopYoung.concat(peopOld))
-	return [{'people check yong': peopYoung},{'people check old': peopOld},{'other people': peopOther}]
+  let peopCheckList = peop.filter(function(el) {
+    return (
+      el.status.indexOf("some") > -1 ||
+      el.status.indexOf("apple") > -1 ||
+      el.status.indexOf("green") > -1 ||
+      el.status.indexOf("white") > -1
+    );
+  });
+  let peopYoung = peopCheckList.filter(function(year) {
+    return year.age < 35;
+  });
+  let peopOld = peopCheckList.filter(function(year) {
+    return year.age > 35;
+  });
+  let peopOther = peop.diff(peopYoung.concat(peopOld));
+  return [
+    { "people check yong": peopYoung },
+    { "people check old": peopOld },
+    { "other people": peopOther }
+  ];
 }
-console.log(sortPeople(people))
+console.log(sortPeople(people));
 
-
-const statusArray = ['some', 'apple', 'green', 'white'];
+const statusArray = ["some", "apple", "green", "white"];
 
 // 04. some
 function checkStatus(status) {
@@ -753,17 +469,17 @@ function checkStatus(status) {
 }
 
 function sortPeople2(people) {
-  let good = []
-  let old = []
-  let other = []
+  let good = [];
+  let old = [];
+  let other = [];
 
-  people.forEach((el) => {
+  people.forEach(el => {
     if (checkStatus(el.status)) {
-      el.age < 35 ? good.push(el) : old.push(el)
+      el.age < 35 ? good.push(el) : old.push(el);
     } else {
       other.push(el);
     }
-  })
-  return [good, old, other];  
+  });
+  return [good, old, other];
 }
 console.log(sortPeople2(people));
